a4b526851c340d298043a605e0e72865d456db1f
Daniel <daniel.ortizp91@gmail.com>
2017-10-31 18:36:43 -0600

    Button component

--- components/Button/index.js
+++ components/Button/index.js
@@ -2,20 +2,58 @@
 import * as React from 'react';
 
 type Props = {
-  className: string,
+  className?: string,
+  type?: 'main' | 'secondary' | 'discrete' | 'link',
 
-  text: string,
-  size: 'small' | 'regular' | 'big' | 'huge',
-  icon: React.Node,
+  size?: 'small' | 'regular' | 'big' | 'huge',
+  icon?: React.Node,
+  iconSide?: 'left' | 'right',
+  disabled?: boolean,
+
+  onClick?: Function,
+
+  children: React.Children | Array<React.Children>,
 };
+
+type Default = {
+  className: string,
+  type: 'main' | 'secondary' | 'discrete' | 'link',
+  size: 'small' | 'regular' | 'big' | 'huge',
+  iconSide: 'left' | 'right',
+
+  onClick: Function,
+  disabled: boolean,
+}
 
 /**
  * Componente responsable solamente de desplegar el sidebar
  */
-export default class Button extends React.Component<void, Props, void> {
+export default class Button extends React.Component<Default, Props, void> {
+  static defaultProps = {
+    className: '',
+    type: 'discrete',
+    size: 'regular',
+    iconSide: 'left',
+
+    onClick: () => {},
+    disabled: false,
+  };
+
   render() {
+    const {
+      className, type, size, icon, iconSide, disabled, onClick, children,
+    } = this.props;
     return (
-      null
+      <button
+        onClick={onClick}
+        disabled={disabled}
+        className={
+          `${type || ''} ${size || ''} ${icon ? 'iconned' : ''} ${iconSide === 'right' ? 'invert' : ''} ${className || ''}`
+        }
+      >
+        {icon}
+        {children}
+      </button>
     );
   }
 }
