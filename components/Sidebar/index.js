950629ffe97588348edb14fc37d0d63389b3b5f2
Daniel <daniel.ortizp91@gmail.com>
2017-10-31 18:37:08 -0600

    Sidebar or Topbar

--- components/Sidebar/index.js
+++ components/Sidebar/index.js
@@ -4,7 +4,7 @@ import * as React from 'react';
 type Props = {
   className: string,
   show: boolean, // Debería de mostrarse?
-  closeIcon: React.Node, // Icono de hamburguesa como componente de react
+  closeIcon?: React.Node, // Icono de hamburguesa como componente de react
   imagotype: string, // Imagotipo del cliente
   anchors: Array<{ text: string, url: string }>, // Links que van hasta arriba
 
@@ -12,10 +12,22 @@ type Props = {
   onAnchorClick: Function, // Función a ejecutar cuando se da clic en un link (puede ser push)
 };
 
+type Default = {
+  closeIcon: React.Node, // Icono de hamburguesa como componente de react
+};
+
 /**
  * Componente responsable solamente de desplegar el sidebar
  */
-export default class Sidebar extends React.Component<void, Props, void> {
+export default class Sidebar extends React.Component<Default, Props, void> {
+  static defaultProps: Default = {
+    closeIcon: (
+      <img
+        src='https://image.flaticon.com/icons/svg/463/463065.svg'
+        alt='Close Icon'
+      />
+    ),
+  }
   render() {
     const anchor = this.props.anchors.map((element, index) => (
       <div className='anchor' key={index} onClick={() => this.props.onAnchorClick(element.url)} role='link' tabIndex={0}>
--- components/Topbar/index.js
+++ components/Topbar/index.js
@@ -1,15 +1,16 @@
 // @flow
+/* eslint no-console: [0, {}] */
 import * as React from 'react';
 
 export type StoreProps = {
   className: string,
 
-  burguerIcon: React.Node, // Icono de hamburguesa como componente de react
+  burgerIcon?: React.Node, // Icono de hamburguesa como componente de react
   imagotype: string, // Imagotipo del cliente
   anchors: Array<{ text: string, url: string }>, // Links que van hasta arriba
   avatar: string, // URL de donde se tiene que cargar el avatar
   username: string, // Nombre completo del usuario, pseudonimo o solo username
-  notificationIcon: React.Node, // Icono de notificaciones como componente de react
+  notificationIcon?: React.Node, // Icono de notificaciones como componente de react
 };
 
 export type Actions = {
@@ -19,11 +20,33 @@ export type Actions = {
 
 type Props = StoreProps & Actions;
 
+type Default = {
+  burgerIcon: React.Node, // Icono de hamburguesa como componente de react
+  notificationIcon: React.Node, // Icono de notificaciones como componente de react
+};
 /**
  * Componente responsable solamente de desplegar el topbar
  */
-export default class Topbar extends React.Component<void, Props, void> {
+export default class Topbar extends React.Component<Default, Props, void> {
+  static defaultProps = {
+    burgerIcon: (
+      <img
+        // src='https://image.flaticon.com/icons/svg/462/462998.svg'
+        src={require('../../assets/menu.svg')} // eslint-disable-line global-require
+        alt='Burguer Icon'
+      />
+    ),
+    notificationIcon: (
+      <img
+        src={require('../../assets/bell.svg')} // eslint-disable-line global-require
+        // src='https://image.flaticon.com/icons/svg/462/462944.svg'
+        alt='Notification Icon'
+      />
+    ),
+  }
+
   render() {
+    console.log(this.props.notificationIcon);
     const anchor = this.props.anchors.map((element, index) => (
       <div className='anchor' key={index} onClick={() => this.props.onAnchorClick(element.url)} role='link' tabIndex={0}>
         <span>{ element.text }</span>
@@ -41,7 +64,7 @@ export default class Topbar extends React.Component<void, Props, void> {
                 role='button'
                 tabIndex={0}
               >
-                { this.props.burguerIcon }
+                { this.props.burgerIcon }
               </div>
               <img
                 className='imagotype'
@@ -53,6 +76,7 @@ export default class Topbar extends React.Component<void, Props, void> {
               { anchor }
             </div>
           </div>
+          {/* TODO: Notification */}
           <div className='column column-avatar'>
             <div
               className='avatar'
