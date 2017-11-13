df4cbc94941793f5d48ef826dc3e33e343dbaf53
Daniel <daniel.ortizp91@gmail.com>
2017-11-13 10:59:46 -0600

    Inputs fixes for editable and disabled, and messages

--- components/Inputs/input.jsx
+++ components/Inputs/input.jsx
@@ -1,11 +1,11 @@
 // @flow
 import * as React from 'react';
+import autobind from 'autobind-decorator';
 import Label from './label';
 
 type Props = {
   id: string,
   label?: string,
-  error?: boolean,
   message?: string,
   messageType?: 'info' | 'subtle' | 'success' | 'error',
   className?: string,
@@ -15,13 +15,14 @@ type Props = {
   leftIcon?: string,
   rightIcon?: string,
 
-  type?: 'text' | 'number',
+  type?: 'text' | 'number' | 'password',
   required?: boolean,
+  editable?: boolean,
+  disabled?: boolean,
 };
 
 type Default = {
   label: string,
-  error: boolean,
   className: string,
 
   message: string,
@@ -31,13 +32,14 @@ type Default = {
   rightIcon: string,
 
   required: boolean,
-  type: 'text' | 'number',
+  editable: boolean,
+  disabled: boolean,
+  type: 'text' | 'number' | 'password',
 }
 
 export default class Checkbox extends React.Component<Props, void> {
   static defaultProps: Default = {
     label: '',
-    error: false,
     className: '',
 
     message: '',
@@ -47,16 +49,64 @@ export default class Checkbox extends React.Component<Props, void> {
     rightIcon: '',
 
     required: false,
+    disabled: false,
+    editable: true,
     type: 'text',
   };
 
-  constructor(props: Props) {
-    super(props);
-    (this: any).onValueChange = this.onValueChange.bind(this);
-  }
-
+  @autobind
   onValueChange(event: SyntheticEvent<*>) {
     this.props.onChange(event.target.value);
+  }
+
+  @autobind
+  renderRightIcon() {
+    const {
+      label,
+      required,
+      message,
+      messageType,
+      rightIcon,
+    } = this.props;
+
+    return (
+      <span
+        className={
+          `symbolicon ${
+            !label && message
+            ? messageType || ''
+            : required && !label
+              ? 'required'
+              : rightIcon || 'none'
+            }`
+        }
+        title={
+          !label && message
+            ? message
+            : required
+              ? 'Required field'
+              : null
+        }
+      />
+    );
+  }
+
+  @autobind
+  renderMessages() {
+    const {
+      label,
+      required,
+      message,
+      messageType,
+    } = this.props;
+    if (label && (message || required)) {
+      return (
+        <small className={`symbolicon ${messageType || 'required'}`}>
+          {message || 'Required field'}
+        </small>
+      );
+    }
+    return null;
   }
 
   render() {
@@ -67,23 +117,27 @@ export default class Checkbox extends React.Component<Props, void> {
       type,
       value,
       className,
-      error,
 
       message,
       messageType,
 
       leftIcon,
       rightIcon,
+      editable,
+      disabled,
 
       ...otherProps
     } = this.props;
 
+
     return (
-      <div className={`input-atom ${className || ''}`}>
+      <div className={`input-atom ${!editable ? 'blocked' : ''} ${className || ''}`}>
         { label ? <Label htmlFor={id}>{label}</Label> : null}
         <div
           className={
-            `input-group ${leftIcon ? 'l-icon' : ''} ${required || rightIcon ? 'r-icon' : ''} ${error ? 'error' : ''}`
+            `input-group ${leftIcon ? 'l-icon' : ''} ${
+              required || rightIcon || (!label && message) ? 'r-icon' : ''
+            } ${messageType === 'error' ? 'error' : ''}`
           }
         >
           <input
@@ -91,12 +145,12 @@ export default class Checkbox extends React.Component<Props, void> {
             type={type} id={id}
             value={value} onChange={this.onValueChange}
             required={required}
+            disabled={disabled || !editable}
           />
           <span className={`symbolicon ${leftIcon || 'none'}`} />
-          <span className={`symbolicon ${required && !label ? 'required' : rightIcon || 'none'}`} />
+          {this.renderRightIcon()}
         </div>
-        { label && required ? <small className='symbolicon required'>Required field</small> : null}
-        { message ? <small className={messageType}>{message}</small> : null}
+        {this.renderMessages()}
       </div>
     );
   }
