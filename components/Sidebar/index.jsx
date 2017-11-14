ef1764b39b670a7f91285c079bc55612b6c6df88
Daniel <daniel.ortizp91@gmail.com>
2017-11-14 10:37:05 -0600

    Sidebar simple fix

--- components/Sidebar/index.jsx
+++ components/Sidebar/index.jsx
@@ -1,18 +1,34 @@
 // @flow
 import * as React from 'react';
+import autobind from 'autobind-decorator';
 
-type Props = {
-  className: string,
+export type AnchorType = {
+  text: string,
+  url: string,
+  icon: string,
+  exact: boolean,
+  active?: boolean,
+};
+
+export type StoreProps = {
+  className?: string,
   show: boolean, // Debería de mostrarse?
   closeIcon?: React.Node, // Icono de hamburguesa como componente de react
   imagotype: string, // Imagotipo del cliente
-  anchors: Array<{ text: string, url: string }>, // Links que van hasta arriba
+  anchors: Array<AnchorType>, // Links que van hasta arriba
+  location?: {
+    pathname: string,
+  },
+};
 
+export type Actions = {
   onCloseClick: Function, // Función a ejecutar en cuanto se da clic en el icono de hamburguesa
   onAnchorClick: Function, // Función a ejecutar cuando se da clic en un link (puede ser push)
   renderAnchor?: Function,
 };
 
+type Props = StoreProps & Actions;
+type State = {};
 type Default = {
   closeIcon: React.Node, // Icono de hamburguesa como componente de react
   renderAnchor: Function,
@@ -21,7 +37,7 @@ type Default = {
 /**
  * Componente responsable solamente de desplegar el sidebar
  */
-export default class Sidebar extends React.Component<Props, void> {
+export default class Sidebar extends React.Component<Props, State> {
   static defaultProps: Default = {
     closeIcon: (
       <img
@@ -29,12 +45,13 @@ export default class Sidebar extends React.Component<Props, void> {
         alt='Close Icon'
       />
     ),
-    renderAnchor: (element, index) => (
+
+    renderAnchor: (element: AnchorType, index: number) => (
       <div
         className='anchor'
         key={index}
-        onClick={() => this.props.onAnchorClick(element.url)}
-        onKeyPress={() => this.props.onAnchorClick(element.url)}
+        onClick={() => this.onAnchorClick(element.url)}
+        onKeyPress={() => this.onAnchorClick(element.url)}
         role='link'
         tabIndex={0}
       >
@@ -43,24 +60,47 @@ export default class Sidebar extends React.Component<Props, void> {
     ),
   }
 
+  @autobind
+  onAnchorClick(element: AnchorType): void {
+    this.props.onAnchorClick(element.url);
+  }
+
+  @autobind
+  isActive(element: AnchorType, location: { pathname: string }): boolean {
+    return (element.exact && location.pathname === `/${element.url}`)
+      || (!element.exact && location.pathname.startsWith(`/${element.url}`));
+  }
+
   render() {
-    const anchor = this.props.anchors.map(this.props.renderAnchor);
+    const {
+      anchors,
+      className,
+      imagotype,
+      location,
+      onCloseClick,
+      renderAnchor,
+      show,
+    } = this.props;
+    const anchorsRender = anchors.map((e, i) => renderAnchor({
+      ...e,
+      active: this.isActive(e, location),
+    }, i));
 
     return (
-      <div className={this.props.className + (this.props.show ? ' show' : ' hidden')}>
+      <div className={`${className || ''} ${show ? ' show' : ' hidden'}`}>
         <div className='head'>
           <div
             className='close-container'
-            onClick={() => { this.props.onCloseClick(); }}
-            onKeyPress={() => { this.props.onCloseClick(); }}
+            onClick={() => { onCloseClick(); }}
+            onKeyPress={() => { onCloseClick(); }}
             role='button' tabIndex={0}
           >
             { this.props.closeIcon }
           </div>
-          <img className='imagotype' src={this.props.imagotype} alt='Imagotype' />
+          <img className='imagotype' src={imagotype} alt='Imagotype' />
         </div>
         <div className='body'>
-          {anchor}
+          {anchorsRender}
         </div>
       </div>
     );
