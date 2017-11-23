c18fdfbe3a4b15db714e7b45b79b3079e1f9c9d7
Daniel <daniel.ortizp91@gmail.com>
2017-11-23 13:23:10 -0600

    Pages improved

--- components/Pages/page.jsx
+++ components/Pages/page.jsx
@@ -1,41 +1,60 @@
 // @flow
 import * as React from 'react';
+import autobind from 'autobind-decorator';
+import Button from '../Button';
 
 export type StoreProps = {
-  title: string,
-  backComponent: React.Node,
   className?: string,
+  title: string,
+  backText?: string,
   type?: string,
   topComponent?: React.Node | Array<React.Node>,
   children: React.Node | Array<React.Node>,
   type?: 'separated-rows',
 };
-export type Actions = {};
+export type Actions = {
+  onBackClick: Function,
+};
 
 type Props = StoreProps & Actions;
-type State = {};
+type State = {
+  close: boolean,
+};
 type Default = {
   className: string,
   type: string,
+  backText: string,
 };
 
 export default class Page extends React.PureComponent<Props, State> {
   static defaultProps: Default = {
     className: '',
     type: 'separated-rows',
+    backText: 'go back',
   };
 
-  state: State = {};
+  state: State = {
+    close: false,
+  };
+
+  @autobind
+  onBackClick() {
+    const { onBackClick } = this.props;
+    this.setState({ close: true }, () => setTimeout(onBackClick, 300));
+  }
 
   render() {
     const {
-      backComponent, className, children, title, topComponent, type,
+      backText, className, children, title, topComponent, type,
     } = this.props;
+    const { close } = this.state;
 
     return (
-      <div className={`page ${className || ''}`}>
+      <div className={`page ${close ? 'close' : 'open'} ${className || ''}`}>
         <div className='top'>
-          {backComponent}
+          <Button type='link' onClick={this.onBackClick}>
+            {backText}
+          </Button>
         </div>
         <div className='header'>
           <h3>{title}</h3>
