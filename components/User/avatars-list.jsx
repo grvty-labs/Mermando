5c8861aec2fb9a7bb1025a5bb97899453ce985e8
Daniel <daniel.ortizp91@gmail.com>
2017-11-29 20:07:55 -0600

    Avatars fix

--- components/User/avatars-list.jsx
+++ components/User/avatars-list.jsx
@@ -6,16 +6,24 @@ import type { AvatarProps } from './avatar';
 
 type AvatarsListProps = {
   avatars: Array<AvatarProps>,
+  overlap?: boolean,
 };
 
 type Props = AvatarsListProps;
+type Default = {
+  overlap: boolean,
+};
 type State = {};
 
 export default class AvatarsList extends React.PureComponent<Props, State> {
+  static defaultProps: Default = {
+    overlap: true,
+  };
+
   render() {
-    const { avatars } = this.props;
+    const { avatars, overlap } = this.props;
     return (
-      <div className='avatars-list'>
+      <div className={`avatars-list ${overlap ? 'overlap' : ''}`}>
         { avatars.map((e, i) => (
           <Avatar key={i} hover='simple' {...e} />
         )) }
