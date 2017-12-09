98976e483628d7608c938eec50d6e34d85d0ee4a
Daniel <daniel.ortizp91@gmail.com>
2017-12-08 20:03:36 -0600

    Added the Sticky component

--- components/Pages/index.js
+++ components/Pages/index.js
@@ -2,12 +2,17 @@
 import Lightbox from './lightbox';
 import Modal from './modal';
 import Page from './page';
+import Sticky from './sticky';
 import TabbedPage from './tabbed';
 
 import type {
   StoreProps as PageProps,
   Actions as PageActions,
 } from './page';
+import type {
+  StoreProps as StickyProps,
+  Actions as StickyActions,
+} from './sticky';
 import type {
   Zone as TabbedPageZone,
   StoreProps as TabbedPageProps,
@@ -19,11 +24,14 @@ export {
   Modal,
   Page,
   TabbedPage,
+  Sticky,
 };
 
 export type {
   PageProps,
   PageActions,
+  StickyProps,
+  StickyActions,
   TabbedPageProps,
   TabbedPageActions,
   TabbedPageZone,
