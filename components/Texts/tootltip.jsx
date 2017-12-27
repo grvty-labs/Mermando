738b838e892ca104db9ac16e9bf63c37b77ce8dc
Daniel <daniel.ortizp91@gmail.com>
2017-12-27 00:50:08 -0600

    Waddles updated

---
+++ components/Texts/tootltip.jsx
@@ -0,0 +1,17 @@
+// @flow
+import * as React from 'react';
+
+export type StoreProps = {
+
+};
+
+export type Actions = {};
+
+type Props = StoreProps & Actions;
+type State = {
+
+};
+
+export default class Tooltip extends React.Component<Props, State> {
+  
+}
